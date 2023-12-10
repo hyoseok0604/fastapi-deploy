ceb2ef210b0453adae16db6d99bf95ffbcfb4537
hyoseok0604 <janghs0604@gmail.com>
2023-12-11 02:59:32 +0900

    ignore extra fields

--- shared/settings.py
+++ shared/settings.py
@@ -34,6 +34,7 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         env_nested_delimiter="_",
+        extra="ignore",
     )
 
     postgres: PostgresSettings
